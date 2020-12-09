c92b4a687eb1b108d08512bf988aad69a6799852
June8Yildirim <66858810+June8Yildirim@users.noreply.github.com>
2020-12-09 14:49:48 -0500

    12/09

---
+++ scripts.js
@@ -0,0 +1,6 @@
+window.alert("For your safety from the COVID-19 please follow guidelines!");
+alert("For your safety from the COVID-19 please follow guidelines!")
+
+function warn() {
+    alert("For your safety from the COVID-19 please follow guidelines!");
+}
