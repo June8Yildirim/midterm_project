5d6ebf0097f6b30eb4e795813e1d67354094efc2
June8Yildirim <66858810+June8Yildirim@users.noreply.github.com>
2020-12-18 04:38:37 -0500

    12-18 updates

--- scripts.js
+++ scripts.js
@@ -4,3 +4,50 @@ alert("For your safety from the COVID-19 please follow guidelines!")
 function warn() {
     alert("For your safety from the COVID-19 please follow guidelines!");
 }
+
+// function hideIt(event) {
+//     event.target.style.display = "none";
+// }
+
+// function setUp() {
+//     document.querySelector("#hiddenNa").addEventListener("click", hideIt);
+// }
+
+// addEventListener("load", setUp);
+
+
+// $(document).ready(function() {
+//     $("a").click(function(event) {
+//         alert("You clicked on a link to " + this.href);
+//     });
+// });
+
+
+// function validationEmail(event) {
+//     let ValidateEmail = document.querySelector("email_val")
+//     let checking_sign = email_val.indexOf("@");
+//     if (checking_sign == -1) {
+//         alert("your mail address is not valid")
+//     } else {
+//         alert("your mail address is a valid")
+//     }
+// }
+
+function hideIt(event) {
+    event.target.style.display = "none";
+}
+
+function setUp() {
+    document.querySelector("#testScript").addEventListener("click", hideIt);
+}
+addEventListener("load", setUp)
+
+// function hideIt(event) {
+//     event.target.style.display = "none";
+// }
+
+// function setUp() {
+//     document.querySelector("#testScript2").addEventListener("click", hideIt);
+// }
+
+// addEventListener("load", setUp)
